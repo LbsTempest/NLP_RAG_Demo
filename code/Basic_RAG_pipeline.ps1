ede5dde7094aa896e9c6ed9e93755e7952afb679
Tempest <l1641101764@gmail.com>
2024-05-30 20:43:30 +0800

    add powershell scripts

---
+++ code/Basic_RAG_pipeline.ps1
@@ -0,0 +1,6 @@
+E:\pythonProject\Miniforge\envs\rag\python.exe Basic_RAG_pipeline.py `
+    --model_type api `
+    --api_key YpXLq4hipFP2I5iM4sYGY5J7 `
+    --secret_key UTl9Sj8POrnlNgNsS40TeNY3sb4tFxXR `
+    --embedding_model_path BAAI/bge-small-en-v1.5 `
+    --data_path '../data/Elon.txt'
---
+++ code/Sentence_window_retrieval.ps1
@@ -0,0 +1,1 @@
+python Sentence_window_retrieval.py --model_type api --api_key YpXLq4hipFP2I5iM4sYGY5J7 --secret_key UTl9Sj8POrnlNgNsS40TeNY3sb4tFxXR --embedding_model_path ../BAAI/bge-small-en-v1.5 --data_path '../data/Elon.txt'
