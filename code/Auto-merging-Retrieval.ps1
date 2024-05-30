b9fa5c27ffd87a1c66a3dc96bd20bcc7aeaf8b39
Tempest <l1641101764@gmail.com>
2024-05-31 01:22:27 +0800

    add args value

---
+++ code/Auto-merging-Retrieval.ps1
@@ -0,0 +1,7 @@
+python Auto-merging_Retrieval.py `
+--model_type api `
+--api_key YpXLq4hipFP2I5iM4sYGY5J7 `
+--secret_key UTl9Sj8POrnlNgNsS40TeNY3sb4tFxXR `
+--embedding_model_path ../BAAI/bge-small-en-v1.5 `
+--rerank_model_path ../BAAI/bge-reranker-base `
+--data_path '../data/Elon.txt'
